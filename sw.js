bf9d66d570b7255e5f11861d7754a445cd81c3fa
hemricciinstintosuperior-rcci <hemricci.instintosuperior@gmail.com>
2026-02-21 07:14:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("app-mapa").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
-    })
-  );
-});
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
-});
